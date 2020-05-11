fcdaa02fb0a90077aa598504e6201f1625214104
mathis <mathis.pati@gmail.com>
2020-05-11 15:26:55 +0200

    premiere carte ou deuxieme carte

---
+++ script.js
@@ -0,0 +1,26 @@
+let cartes = document.querySelectorAll('.cartes');
+let cartesRetournees = false;
+let premiereCarte, deuxiemeCarte;
+
+// révèle la couleur du caré
+for (const carte of cartes) {
+    carte.addEventListener('click', retournerCartes)
+}
+
+function retournerCartes(e) {
+    
+    this.classList.remove('colorHidden');
+
+    if (!cartesRetournees) { // Si c'est la première carte qu'on retourne
+        cartesRetournees = true;
+        premiereCarte = e.currentTarget;
+        console.log(premiereCarte.className);
+    }
+    else {  // Si c'est la deuxième carte qu'on retourne
+        cartesRetournees = false;
+        deuxiemeCarte = e.currentTarget;
+        console.log(deuxiemeCarte.getAttribute('data-color'));
+    
+       
+    }
+}
